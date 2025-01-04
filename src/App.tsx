d9c47027cbcce8f3121cd9c7c44e786898317868
AsitChandraRoy <asitchandraroy@gmail.com>
2025-01-04 17:40:44 +0600

    some style changes in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -66,7 +66,7 @@ const App: React.FC = () => {
   return (
     <div className="container mx-auto mt-10 p-5 text-center">
       <h1 className="text-3xl text-violet-500 font-bold mb-5">Task Management Application</h1>
-      <h1 className="text-green-500 mb-">Organize your tasks efficiently and stay productive!</h1>
+      <p className="text-green-500 mb-8">Organize your tasks efficiently and stay productive!</p>
       <AddTask addTask={addTask} editTask={editTaskIndex !== null ? tasks[editTaskIndex] : null} />
       <TaskList
         tasks={tasks}
